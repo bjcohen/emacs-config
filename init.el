348cbd16ffa003a958a10d5f542bdca7261213be
Ben Cohen <ben@cohen-family.org>
2020-05-25 16:43:31 -0400

    bootstrap straight

--- init.el
+++ init.el
@@ -2,6 +2,19 @@
 ;;; Commentary:
 ;;; Code:
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 ;;; Customize Section
 
 (custom-set-variables
