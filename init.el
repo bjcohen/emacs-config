278e9c4f6323b8c9c292c50c12591da5f8282a78
Ben Cohen <ben@cohen-family.org>
2020-06-17 15:24:29 -0700

    move custom.el out

--- init.el
+++ init.el
@@ -514,27 +514,16 @@
  'org-babel-load-languages
  '((python . t)))
 
+(define-key visual-line-mode-map (kbd "M-n") #'next-logical-line)
+(define-key visual-line-mode-map (kbd "M-p") #'previous-logical-line)
+
 (let ((init-local (concat user-emacs-directory "init-local.el")))
   (when (file-exists-p init-local)
     (load-file init-local)))
 
-(define-key visual-line-mode-map (kbd "M-n") #'next-logical-line)
-(define-key visual-line-mode-map (kbd "M-p") #'previous-logical-line)
-
-;;; Customize Section
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file)
+  (load-file custom-file))
 
 (provide 'init)
 ;;; init.el ends here
