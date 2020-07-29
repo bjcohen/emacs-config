dc3fc8c993ae4ead21b2d5d52470145874c52049
Ben Cohen <ben@cohen-family.org>
2020-07-28 22:09:22 -0700

    new keybinds!

--- init.el
+++ init.el
@@ -43,6 +43,9 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 
+(global-set-key (kbd "C-o") #'other-window)
+(global-set-key (kbd "C-w") #'backward-kill-word)
+
 (use-package yascroll
   :config
   (global-yascroll-bar-mode 1))
