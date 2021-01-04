2085c6388be9cd276894e1d8549a0f7645ce81b3
Ben Cohen <ben@cohen-family.org>
2021-01-04 14:07:18 -0500

    compat for define-obsolete-*-alias in latest version of 28.0

--- init.el
+++ init.el
@@ -27,11 +27,76 @@
 (autoload #'straight-x-pull-all "straight-x")
 (autoload #'straight-x-freeze-versions "straight-x")
 
+(use-package el-patch)
+
+(if (version< "28.0" emacs-version)
+    (progn
+      (el-patch-defmacro define-obsolete-variable-alias ( obsolete-name current-name
+                                                          (el-patch-add &optional) when
+                                                          (el-patch-remove &optional) docstring)
+        "Make OBSOLETE-NAME a variable alias for CURRENT-NAME and mark it obsolete.
+
+WHEN should be a string indicating when the variable was first
+made obsolete, for example a date or a release number.
+
+This macro evaluates all its parameters, and both OBSOLETE-NAME
+and CURRENT-NAME should be symbols, so a typical usage would look like:
+
+  (define-obsolete-variable-alias 'foo-thing 'bar-thing \"27.1\")
+
+This macro uses `defvaralias' and `make-obsolete-variable' (which see).
+See the Info node `(elisp)Variable Aliases' for more details.
+
+If CURRENT-NAME is a defcustom or a defvar (more generally, any variable
+where OBSOLETE-NAME may be set, e.g. in an init file, before the
+alias is defined), then the define-obsolete-variable-alias
+statement should be evaluated before the defcustom, if user
+customizations are to be respected.  The simplest way to achieve
+this is to place the alias statement before the defcustom (this
+is not necessary for aliases that are autoloaded, or in files
+dumped with Emacs).  This is so that any user customizations are
+applied before the defcustom tries to initialize the
+variable (this is due to the way `defvaralias' works).
+
+For the benefit of Customize, if OBSOLETE-NAME has
+any of the following properties, they are copied to
+CURRENT-NAME, if it does not already have them:
+`saved-value', `saved-variable-comment'."
+        (declare (doc-string 4))
+        `(progn
+           (defvaralias ,obsolete-name ,current-name ,docstring)
+           ;; See Bug#4706.
+           (dolist (prop '(saved-value saved-variable-comment))
+             (and (get ,obsolete-name prop)
+                  (null (get ,current-name prop))
+                  (put ,current-name prop (get ,obsolete-name prop))))
+           (make-obsolete-variable ,obsolete-name ,current-name (el-patch-wrap 1 2 (if ,when ,when "0.0")))))
+      (el-patch-defmacro define-obsolete-function-alias ( obsolete-name current-name
+                                                          (el-patch-add &optional) when
+                                                          (el-patch-remove &optional) docstring)
+        "Set OBSOLETE-NAME's function definition to CURRENT-NAME and mark it obsolete.
+
+\(define-obsolete-function-alias \\='old-fun \\='new-fun \"22.1\" \"old-fun's doc.\")
+
+is equivalent to the following two lines of code:
+
+\(defalias \\='old-fun \\='new-fun \"old-fun's doc.\")
+\(make-obsolete \\='old-fun \\='new-fun \"22.1\")
+
+WHEN should be a string indicating when the function was first
+made obsolete, for example a date or a release number.
+
+See the docstrings of `defalias' and `make-obsolete' for more details."
+        (declare (doc-string 4))
+        `(progn
+           (defalias ,obsolete-name ,current-name ,docstring)
+           (make-obsolete ,obsolete-name ,current-name (el-patch-wrap 1 2 (if ,when ,when "0.0")))))
+      ))
+
 ;;; General Config
 
 (use-package diminish)
 (use-package bind-key)
-(use-package el-patch)
 
 (use-package exec-path-from-shell
   :if (memq window-system '(mac ns))
