8cdb5e4773db47d675133f095e08ef4ffbe4dfd6
Ben Cohen <ben@cohen-family.org>
2025-02-09 19:36:34 -0500

    -codeium, +aider

--- init.el
+++ init.el
@@ -1035,6 +1035,24 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
 
 (use-package tempel-collection)
 
+(use-package aider
+  :straight (:host github :repo "tninja/aider.el" :files ("aider.el"))
+  :config
+  ;; Use claude-3-5-sonnet cause it is best in aider benchmark
+  (setq aider-args '("--model" "openrouter/anthropic/claude-3.5-sonnet"))
+  ;; (setenv "ANTHROPIC_API_KEY" anthropic-api-key)
+  ;; Or use chatgpt model since it is most well known
+  ;; (setq aider-args '("--model" "o3-mini"))
+  ;; (setenv "OPENAI_API_KEY" <your-openai-api-key>)
+  ;; Or use gemini v2 model since it is very good and free
+  ;; (setq aider-args '("--model" "gemini/gemini-exp-1206"))
+  ;; (setenv "GEMINI_API_KEY" <your-gemini-api-key>)
+  ;; Or use your personal config file
+  ;; (setq aider-args `("--config" ,(expand-file-name "~/.aider.conf.yml")))
+  ;; ;;
+  ;; Optional: Set a key binding for the transient menu
+  (global-set-key (kbd "C-c a") 'aider-transient-menu))
+
 (use-package tab-bar
   :bind
   (("<s-tab>" . tab-next)
